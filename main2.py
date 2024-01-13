d56ac59f4428020f0e62c2f5f8b2e93abc2db001
skream20 <118281774+Skream20@users.noreply.github.com>
2024-01-13 13:44:11 +0100

    in_dev

--- main2.py
+++ main2.py
@@ -1,4 +1,6 @@
 import random
+
+
 def calculer_probabilite():
     try:
         value = int(input("Entrez une valeur (entre 1 - 75) : "))
@@ -6,7 +8,9 @@ def calculer_probabilite():
 
         if 1 <= value <= 75:
             proba = value / nb_max * 100
-            print(f"La probabilité d'obtenir la carte {value} dans un deck de {nb_max} cartes est : {proba:.2f}%")
+            print(
+                f"La probabilité d'obtenir la carte {value} dans un deck de {nb_max} cartes est : {proba:.2f}%"
+            )
         else:
             print("Veuillez entrer une valeur entre 1 et 75.")
 
@@ -14,21 +18,13 @@ def calculer_probabilite():
         print("Veuillez entrer des valeurs valides.")
         no_yes()
 
-def no_yes():
-    answer = input("do you want to restart? [y/n]")
-    if answer == "y" or answer == "yes":
-        calculer_probabilite()
-    elif answer == "n" or answer == "no":
-        exit()
-    else:
-        print("input error!")
-        
+
 def hand_tester(taille_main):
     nom_fichier = "list.txt"
     try:
-        with open(nom_fichier, 'r') as file:
-            contenu = file.read().replace('\n', '')
-       
+        with open(nom_fichier, "r") as file:
+            contenu = file.read().replace("\n", "")
+
         valeurs = [int(char) for char in contenu if char.isdigit()]
 
         main_tiree = random.sample(valeurs, min(taille_main, len(valeurs)))
@@ -40,10 +36,20 @@ def hand_tester(taille_main):
         print(f"Une erreur s'est produite : {error}")
 
 
+# restart function
+def no_yes():
+    answer = input("do you want to restart? [y/n]")
+    if answer == "y" or answer == "yes":
+        calculer_probabilite()
+    elif answer == "n" or answer == "no":
+        exit()
+    else:
+        print("input error!")
+
+
 def affichage():
-    #hand_tester(5)
+    # hand_tester(5)
     print(calculer_probabilite())
-    
+
+
 print(affichage())
-
-
